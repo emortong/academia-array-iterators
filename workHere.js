c40e2a0e57bfa798d9dcbab01c8213b74409e877
Javier Salazar <javiertifacient@gmail.com>
2019-05-15 17:27:58 -0500

    js exercises

--- workHere.js
+++ workHere.js
@@ -1,10 +1,15 @@
 var billionaires = require('./billionaires.json');
- 
+
 /*
     Get the names of the billionaires that are in their 20s
 */
 
-let twentyYearOldBillionaires = null;
+let twentyYearOldBillionaires = billionaires.filter(x => {
+    if (x.demographics.age > 19 && x.demographics.age < 30) {
+        return x.name
+    };
+});
+
 
 /*
     create a new object with the billionairse year, name and worth rounded to the newarest integer
@@ -15,26 +20,46 @@ let twentyYearOldBillionaires = null;
     }
 */
 
-let roundedWorth = null;
+
+
+let roundedWorth = billionaires.map(x => {
+    return {
+        year: x.year,
+        name: x.name,
+        worth: Math.round(x.wealth['worth in billions'])
+    }
+});
+// console.log(roundedWorth);
+
+
 
 /*
     sum the rounded worth of all the billionaires
 */
 
-let aggregatedWorth = null;
+let aggregatedWorth = billionaires.reduce((prev, curr) => {
+    return Math.round(prev + curr.wealth['worth in billions']);
+}, 0);
 // console.log(aggregatedWorth)
 
 /*
     find the the richest billionaire, make sure the result is in an array
 */
 
-let richest = null
+let richest = billionaires.filter(x => {
+    if (x.rank == 1) {
+        return x.rank
+    }
+})
 
 /*
     find the poorest billioanire, make sure the result is in an array
 */
 
-let poorest = null;
+let poorest = billionaires.sort((a, b) => {
+    return a.rank - b.rank; 
+}).lastIndexOf(billionaires.name)
+
 
 /*
     make a new array containing the richest and the poorest billionaire
